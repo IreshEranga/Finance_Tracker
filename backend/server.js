7b09c03a392ca5b932f030394961be8efc8f0c6f
Magage Iresh Eranga Magage <iresheranga2020al@gmail.com>
2025-02-25 10:39:29 +0530

    CRUD complete for budget and transactions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const budgetRoutes = require('./routes/budgetRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,6 +14,8 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/budgets', budgetRoutes);
+app.use('/api/transactions', transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
